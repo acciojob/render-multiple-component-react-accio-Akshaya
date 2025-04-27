bf0622d14d8905522a70bf5b982b15823e08e1ff
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-04-27 10:47:35 -0700

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,19 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  const projects = [
+    {name:"Dream Portfolio", description: "A website to showcase all my work."},
+    {name:"AI Chatbot", description: "An intelligent chatbot for customer support."},
+  ];
   return (
-    <div id="main"></div>
+    <div id="main" className="ns-wrapper">
+    {projects.map((project, index)=>(
+      <div key={index}>
+        <h1 data-ns-test="project-name">{project.name}</h1>
+        <h6 data-ns-test="project-description">{project.description}</h6>
+      </div>
+    ))}
+    </div>
   )
 }
 
